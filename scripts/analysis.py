a1ad2c331d74643d4bab7e42873f6237c9518c88
isiomailoh <KINGdave@1990.11.21.ISIoma>
2024-03-05 22:16:09 +0100

    merged main into local

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -7,17 +7,16 @@ import seaborn as sns
 file_path = ('/Users/isiomailoh/Desktop/datasets/shopping_trends_project/data/shopping_trends_dataset.csv')
 df = load_data(file_path)
 
-
 colors = ["#89CFF0", "#FF69B4", "#FFD700", "#7B68EE", "#FF4500",
           "#9370DB", "#32CD32", "#8A2BE2", "#FF6347", "#20B2AA",
           "#FF69B4", "#00CED1", "#FF7F50", "#7FFF00", "#DA70D6"]
-plt.figure(figsize = (20, 6))
+plt.figure(figsize=(20, 6))
 
 counts = df["Gender"].value_counts()
 explode = (0, 0.1)
 
-counts.plot(kind = 'pie', fontsize = 12, colors = colors, explode = explode, autopct = '%1.1f%%')
-plt.xlabel('Gender', weight = "bold", color = "#2F0F5D", fontsize = 14, labelpad = 20)
+counts.plot(kind='pie', fontsize=12, colors=colors, explode=explode, autopct='%1.1f%%')
+plt.xlabel('Gender', weight="bold", color="#2F0F5D", fontsize=14, labelpad=20)
 plt.axis('equal')
-plt.legend(labels = counts.index, loc = "best")
+plt.legend(labels=counts.index, loc="best")
 plt.show()
